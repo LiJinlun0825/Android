953cba065a6de2e42c8b6a43838af8c9195d6b9a
lijinlun <460183220@qq.com>
2019-08-04 15:27:10 +0800

    添加主要工程MainActivity模块注释说明

--- app/src/main/java/com/jinlun/edu/MainActivity.java
+++ app/src/main/java/com/jinlun/edu/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * 描述: android知识总结--APP主页面模块
+ * 创建者:  ljl
+ * 创建日期: 2019/8/4 15:24
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
